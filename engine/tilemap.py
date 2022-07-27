aceb6d49ec40dc8ee37cd956b0a7504503389633
WilsonVinson <cprewilsonvinson@gmail.com>
2022-07-27 16:34:41 +0800

    main

--- engine/tilemap.py
+++ engine/tilemap.py
@@ -1,4 +1,29 @@
 
-class TilemapManager:
-    def __init__(self,map,size):
-        pass
+import engine.object as object
+
+class Tilemap:
+    def __init__(self,map,tile_size):
+        self.map = map
+        self.tile_size = tile_size
+        
+        self.tile_list = {}
+        
+        print(map)
+        
+        for y in self.map:
+            for x in range(len(self.map[y])):
+                for z in range(len(self.map[y][x])):
+                    tile_pos = {'x':x,'y':y,'z':z}
+                    tile_id = self.map[y][x][z]
+                    self.tile_list[str(x)+'_'+str(y)+'_'+str(z)] = object.tile.Tile(tile_pos,tile_id,self.tile_size)
+    
+    def renderer(self,surface,pos_offset=[0,0]):
+        self.surface = surface
+        for tile in self.tile_list:
+            self.tile_list[tile].offset = pos_offset
+            self.tile_list[tile].renderer(self.surface)
+            
+    def touch(self,change_tile,pos_offset=[0,0]):
+        for tile in self.tile_list:
+            self.tile_list[tile].offset = pos_offset
+            self.tile_list[tile].touch(change_tile)
--- scene/game.py
+++ scene/game.py
@@ -1,6 +1,7 @@
 
 import pygame
 
+import engine
 import scene
 
 import drivers
@@ -8,14 +9,23 @@ import drivers
 class Game:
     def __init__(self):
         self.window_size = pygame.display.get_surface().get_size()
-        yaml = drivers.yaml.yaml_driver.YamlDriver()
-        self.config = yaml.read(read_file='data/scene/game.yml')
+        yaml_file = drivers.yaml.yaml_driver.YamlDriver()
+        self.config = yaml_file.read(read_file='data/scene/game.yml')
+        
+        json_file = drivers.json.json_driver.JsonDriver()
+        self.map = json_file.read(read_file='data/map')
+        
+        self.size = 4
+        self.tilemap_obj = engine.tilemap.Tilemap(self.map['map'],self.size)
+        
     
     def renderer(self):
         surface = pygame.Surface((self.window_size[0],self.window_size[1])).convert_alpha()
         surface.fill((0,0,0,0))
         
         surface.blit(pygame.image.load('assets/icon/icon.png'), (0,0))
+        
+        self.tilemap_obj.renderer(surface)
         
         return surface
     
